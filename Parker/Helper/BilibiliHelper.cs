23f5bb213e54412d590c0e123dec7e8263f04430
JaffooXu <gaffoo@163.com>
2023-07-17 15:51:21 +0800

    1

--- Parker/Helper/BilibiliHelper.cs
+++ Parker/Helper/BilibiliHelper.cs
@@ -25,22 +25,17 @@ namespace Helper
                 foreach (var item in Uids)
                 {
                     index++;
-                    var url = "https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?host_mid=" + item;
-                    var handler = new HttpClientHandler() { UseCookies = true };
-                    HttpClient httpClient = new(handler);
-                    httpClient.DefaultRequestHeaders.Add("Cookie", @"buvid3=58F14D89-11DF-647C-3AC6-8F15F22713D809834infoc; b_nut=1689419709; b_lsid=A6A7F965_1895942FF52; _uuid=109EA106B5-A6C8-D325-213F-724EACB7D61610296infoc; buvid_fp=d41d346d3dbd68a9e773c53562f86fe6; buvid4=C1503B9C-500E-4F3B-7D70-0B793816F3CF11282-023071519-Mdlkjl0/Y/VGNZRRioOIsA%3D%3D; PVID=1");
-                    httpClient.DefaultRequestHeaders.Add("User-Agent", @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79");
-                    httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua", "\" Not A; Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Microsoft Edge\";v=\"101\"");
-                    httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform", "Windows");
+                    var url = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid=" + item;
+                    HttpClient httpClient = new();
                     var res = await httpClient.GetAsync(url);
                     var content = await res.Content.ReadAsStringAsync();
                     var data = JObject.Parse(content);
                     var code = data["code"]!.ToString();
                     if (code != "0") continue;
-                    var list = JArray.FromObject(data["data"]!["items"]!);
+                    var list = JArray.FromObject(data["data"]!["cards"]!);
                     foreach (JObject blog in list)
                     {
-                        var timestamp = blog["modules"]!["module_author"]!["pub_ts"]!.ToString().Tolong();
+                        var timestamp = blog["desc"]!["timestamp"]!.ToString().Tolong();
                         DateTime createDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
                         createDate = createDate.AddSeconds(timestamp).ToLocalTime();
 
@@ -49,8 +44,8 @@ namespace Helper
                             //可以认定为新发的动态
                             //获取微博类型0-视频，1-图文
                             var type = -1;
-                            if (blog["modules"]!["module_dynamic"]!["major"]!["type"]!.ToString() == "MAJOR_TYPE_DRAW") type = 1;
-                            else if (blog["modules"]!["module_dynamic"]!["major"]!["type"]!.ToString() == "MAJOR_TYPE_ARCHIVE") type = 0;
+                            if (blog["desc"]!["type"]!.ToString() == "2") type = 1;
+                            else if (blog["desc"]!["type"]!.ToString() == "8") type = 0;
                             //需要发送通知则发送通知
                             if (index == 0)
                             {
@@ -62,22 +57,25 @@ namespace Helper
                                     //{
                                     //    mcb.AtAll();
                                     //}
-                                    mcb.Plain($"{blog["modules"]!["module_author"]!["name"]}小破站发动态啦！");
+                                    mcb.Plain($"{blog["desc"]!["user_profile"]!["info"]!["uname"]}B站更新：");
+                                    var card = JObject.Parse(blog["card"]!.ToString());
                                     if (type == 1)
                                     {
                                         //获取第一张图片发送
-                                        var imgList = JArray.FromObject(blog["modules"]!["module_dynamic"]!["major"]!["draw"]!["items"]!);
-                                        mcb.Plain("\n" + blog["modules"]!["module_dynamic"]!["desc"]!["text"]!.ToString()).ImageFromUrl(imgList[0]["src"]!.ToString());
+                                        var imgList = JArray.FromObject(card!["item"]!["pictures"]!);
+                                        mcb.Plain(card!["item"]!["description"]!.ToString()).ImageFromUrl(imgList[0]["img_src"]!.ToString());
                                     }
                                     else if (type == 0)
                                     {
-                                        var videoInfo = blog["modules"]!["module_dynamic"]!["major"]!["archive"]!;
-                                        mcb.Plain("BV号:" + videoInfo["bvid"] + "\n直达:" + videoInfo["jump_url"])
-                                            .Plain("\n" + videoInfo["badge"]!["text"]!).ImageFromUrl(videoInfo["cover"]?.ToString() ?? "");
+                                        mcb.Plain(card!["title"]!.ToString())
+                                            .Plain("\nBV号:" + blog["desc"]!["bvid"] + "\n直达:https://www.bilibili.com/video/" + blog["desc"]!["bvid"])
+                                            .ImageFromUrl(card["pic"]?.ToString() ?? "");
                                     }
                                     else
                                     {
-                                        mcb.Plain("未知类型动态，更多类型通知尽请期待！");
+                                        if(card.ContainsKey("title"))
+                                            mcb.Plain(card!["title"]!.ToString());
+                                        else mcb.Plain(card!["item"]!["description"]!.ToString());
                                     }
                                     if (Const.ConfigModel.WB.forwardGroup)
                                     {
@@ -102,11 +100,12 @@ namespace Helper
                             //保存图片
                             if (type == 1)
                             {
-                                var picList = JArray.FromObject(blog["modules"]!["module_dynamic"]!["major"]!["draw"]!["items"]!);
+                                var card = JObject.Parse(blog["card"]!.ToString());
+                                var picList = JArray.FromObject(card!["item"]!["pictures"]!);
                                 if (picList == null) continue;
                                 foreach (var pic in picList)
                                 {
-                                    var imgUrl = pic["src"]!.ToString();
+                                    var imgUrl = pic["img_src"]!.ToString();
                                     await Weibo.FatchFace(imgUrl);
                                 }
                             }
