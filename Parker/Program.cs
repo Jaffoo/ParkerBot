b2f20a3e9b379375dc3a6e20a69c9bb66cb69b79
JaffooXu <gaffoo@163.com>
2023-08-04 15:27:00 +0800

    修复了一个不常见的bug

--- Parker/Program.cs
+++ Parker/Program.cs
@@ -22,7 +22,7 @@ namespace ParkerBot
             {
                 app.UseDebuggingMode();
                 var path = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot");
-                app.UseEmbeddedFileResource("http", "parkerbot", "wwwroot/pages");
+                app.UseEmbeddedFileResource("http", "parkerbot", "wwwroot/pages", fallback => "index.html");
                 app.UseLocalFileResource("http", "parkerbot.file", path);
                 app.UseDataServiceResource("http", "parkerbot.api");
                 // 指定启动窗体
