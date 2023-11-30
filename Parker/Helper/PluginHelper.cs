dbc56abc99799c53acc87aad6fe42c7256f8bd33
gaffoo <gaffoo@163.com>
2023-11-30 17:21:22 +0800

    1

---
+++ Parker/Helper/PluginHelper.cs
@@ -0,0 +1,141 @@
+﻿using Mirai.Net.Data.Events;
+using Mirai.Net.Data.Messages;
+using PluginServer;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ParkerBot.Helper
+{
+    /// <summary>
+    /// 插件帮助类
+    /// </summary>
+    public class PluginHelper : IDisposable
+    {
+        public PluginHelper()
+        {
+            if (!Directory.Exists(_path)) Directory.CreateDirectory(_path);
+        }
+        public PluginHelper? this[string name]
+        {
+            get { return _plugins.FirstOrDefault(t => t.Name == name); }
+        }
+        private static readonly string _path = Environment.CurrentDirectory + "/Plugin";
+        private readonly List<PluginHelper> _plugins = [];
+        private string? Name { get; set; }
+        private string? Description { get; set; }
+        private IBasePlugin? Plugin { get; set; }
+        private CustomAssemblyLoadContext? DLL { get; set; }
+
+        /// <summary>
+        /// 加载插件
+        /// </summary>
+        public void LoadPlugins()
+        {
+            var files = new DirectoryInfo(_path).GetFiles();
+            foreach (var item in files)
+            {
+                var dll = new CustomAssemblyLoadContext();
+                var assembly = dll.LoadFromAssemblyPath(item.FullName);
+                // 获取 DLL 中的类型
+                Type[] types = assembly.GetTypes();
+                // 创建类型的实例
+                var type = types.FirstOrDefault(t => t.BaseType!.Name == "Object");
+                if (type == null) continue;
+                if (Activator.CreateInstance(type) is not IBasePlugin instance) continue;
+                if (_plugins.Exists(t => t.Name == instance.Name))
+                {
+                    var model = _plugins.FirstOrDefault(t => t.Name == instance.Name);
+                    model?.DLL?.Dispose();
+                    model?.Plugin?.Dispose();
+                    model?.Dispose();
+                    _plugins.Remove(model!);
+                }
+                _plugins.Add(new()
+                {
+                    Name = instance.Name,
+                    Description = instance.Desc,
+                    Plugin = instance,
+                    DLL = dll,
+                });
+            }
+        }
+
+        /// <summary>
+        /// 卸载插件（不会立即卸载完，但是插件不会被执行）
+        /// </summary>
+        /// <param name="name"></param>
+        public void UnloadPlugins(string name)
+        {
+            var plugin = this[name];
+            if (plugin == null) return;
+            plugin.Plugin?.Dispose();
+            plugin.DLL?.Unload();
+            plugin.DLL?.Dispose();
+            plugin.Dispose();
+            _plugins.Remove(plugin);
+        }
+
+        /// <summary>
+        /// 卸载插件（不会立即卸载完，但是插件不会被执行）
+        /// </summary>
+        /// <param name="name"></param>
+        public void UnloadPlugins()
+        {
+            foreach (var item in _plugins)
+            {
+                item.Plugin?.Dispose();
+                item?.DLL?.Unload();
+                item?.DLL?.Dispose();
+                item?.Dispose();
+            }
+            _plugins.Clear();
+        }
+
+        /// <summary>
+        /// 重载插件
+        /// </summary>
+        public void ReloadPlugins()
+        {
+            UnloadPlugins();
+            LoadPlugins();
+        }
+
+        /// <summary>
+        /// 调用插件
+        /// </summary>
+        /// <param name="msgBase"></param>
+        /// <param name="eventBase"></param>
+        public void Excute(MessageReceiverBase? msgBase = null, EventBase? eventBase = null)
+        {
+            foreach (var item in _plugins)
+            {
+                if (item.Plugin == null) continue;
+                item.Plugin.Excute(msgBase, eventBase);
+            }
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    class CustomAssemblyLoadContext : AssemblyLoadContext, IDisposable
+    {
+        public CustomAssemblyLoadContext() : base(isCollectible: true)
+        {
+        }
+        protected override Assembly? Load(AssemblyName name)
+        {
+            return null;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ PluginServer/PluginServer.cs
@@ -0,0 +1,64 @@
+﻿using Mirai.Net.Data.Events;
+using Mirai.Net.Data.Messages;
+using Mirai.Net.Data.Messages.Receivers;
+
+namespace PluginServer
+{
+    public interface IBasePlugin : IDisposable
+    {
+        /// <summary>
+        /// 插件名称
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// 插件描述
+        /// </summary>
+        string Desc { get; }
+
+        /// <summary>
+        /// 执行插件
+        /// </summary>
+        /// <param name="msgBase">消息基类</param>
+        /// <param name="eventBase">事件基类</param>
+        /// <returns></returns>
+        Task Excute(MessageReceiverBase? msgBase = null, EventBase? eventBase = null);
+    }
+    /// <summary>
+    /// 群接口
+    /// </summary>
+    public interface IGroupPlugin : IBasePlugin
+    {
+        /// <summary>
+        /// 群消息接收触发事件
+        /// </summary>
+        /// <param name="gmr">群消息接收器</param>
+        /// <param name="msgText">纯文本消息</param>
+        Task GroupExecute(GroupMessageReceiver gmr, string msgText = "");
+    }
+
+    /// <summary>
+    /// 好友接口
+    /// </summary>
+    public interface IFriendPlugin : IBasePlugin
+    {
+        /// <summary>
+        /// 好友消息接收触发事件
+        /// </summary>
+        /// <param name="fmr">好友消息接收器</param>
+        /// <param name="msgText">纯文本消息</param>
+        Task FriendExecute(FriendMessageReceiver fmr, string msgText = "");
+    }
+
+    /// <summary>
+    /// 事件接口
+    /// </summary>
+    public interface IEventPlugin : IBasePlugin
+    {
+        /// <summary>
+        /// 事件触发事件
+        /// </summary>
+        /// <param name="e">事件基类</param>
+        Task EventExecute(EventBase e);
+    }
+}
